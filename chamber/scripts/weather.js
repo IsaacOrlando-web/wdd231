0e18a4e1af13066b22341a4b5bd30734504b25da
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2025-05-22 18:22:53 -0400

    weather API finished

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,19 +1,34 @@
-const currentWeatherElement = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const myTown = document.querySelector("#town");
+const myDescription = document.querySelector("#description");
+const myTemperature = document.querySelector("#temperature");
+const myGraphic = document.querySelector("#grapgic");
 
-const url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={84ac111db0575ac579b14d913a849da7}";
 
-async function getData(){
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.748&lon=6.637&units=metric&appid=84ac111db0575ac579b14d913a849da7";
+
+async function apiFetch(){
     try{
         const response = await fetch(url);
-        if(!response.ok) throw new Error('Network response was not ok');
-        const data = await response.json();
-        console.log(data);
-
+        if(response.ok){
+            const data = await response.json();
+            displayResults(data);
+        }else{
+            throw new Error('Network response was not ok');
+        } 
     }catch(error){
         console.error('Error xD: ', error);
     }
 }
 
-getData();
+apiFetch();
+
+function displayResults(data) {
+    myTown.innerHTML = data.name;
+    myDescription.innerHTML = data.weather[0].description;
+    myTemperature.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    myGraphic.setAttribute('SRC', iconsrc);
+    myGraphic.setAttribute("alt", data.weather[0].description);
+}
+
